86eea0a6c3b58df0e747c8e55212f5f941bb91d2
emmaf <emrob04@byu.edu>
2025-03-10 16:08:06 -0600

    Phase 4 working on SQL User

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,18 +1,57 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.AuthData;
 import model.UserData;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import static java.sql.Types.NULL;
 
 public class SQLUserDAO implements UserDataAccess{
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM users WHERE username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
+    }
+
+    private UserData readUser(ResultSet rs) throws SQLException {
+        var json = rs.getString("json");
+        UserData user = new Gson().fromJson(json, UserData.class);
+        return user;
     }
 
     @Override
     public ArrayList<UserData> listUsers() throws DataAccessException {
-        return null;
+        var result = new ArrayList<UserData>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM auths";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        result.add(readUser(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return result;
     }
 
     @Override
@@ -23,5 +62,22 @@ public class SQLUserDAO implements UserDataAccess{
     @Override
     public void clear() throws DataAccessException {
 
+    }
+
+    private void execute(String statement, Object... things) throws  DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var prep = conn.prepareStatement(statement)) {
+                for (var i = 0; i < things.length; i++) {
+                    var param = things[i];
+                    if (param instanceof String p) prep.setString(i + 1, p);
+                    else if (param instanceof Integer p) prep.setInt(i + 1, p);
+                    else if (param instanceof ChessGame p) prep.setString(i + 1, p.toString());
+                    else if (param == null) prep.setNull(i + 1, NULL);
+                }
+                prep.executeUpdate();
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 }

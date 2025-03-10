eed7f6a8ebaaf5a3932ebe8485ca0089c24130f5
emmaf <emrob04@byu.edu>
2025-03-10 16:53:43 -0600

    Phase 4 working on SQL games

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,9 +1,15 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
+import model.UserData;
 import service.GameDataShort;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import static java.sql.Types.NULL;
 
 public class SQLGameDAO implements GameDataAccess{
     @Override
@@ -14,7 +20,13 @@ public class SQLGameDAO implements GameDataAccess{
                 ps.setInt(1, gameID);
                 try (var rs = ps.executeQuery()) {
                     if (rs.next()) {
-                        return null;
+                        int id = rs.getInt("gameID");
+                        String whiteUsername = rs.getString("whiteUsername");
+                        String blackUsername = rs.getString("blackUsername");
+                        String gameName = rs.getString("gameName");
+                        String chessGame = rs.getString("chessGame");
+                        ChessGame game = new Gson().fromJson(chessGame, ChessGame.class);
+                        return new GameData(id, whiteUsername, blackUsername, gameName, game);
                     }
                 }
             }
@@ -26,17 +38,55 @@ public class SQLGameDAO implements GameDataAccess{
 
     @Override
     public int createGame(String gameName) throws DataAccessException {
+        var statement = "INSERT INTO games (gameID, whiteUsername, blackUsername, gameName, chessGame) VALUES (?, ?, ?)";
+        execute(statement, null, null, null, gameName, new ChessGame());
         return 0;
     }
 
     @Override
     public ArrayList<GameData> listGames() throws DataAccessException {
-        return null;
+        var result = new ArrayList<GameData>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM games";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        int id = rs.getInt("gameID");
+                        String whiteUsername = rs.getString("whiteUsername");
+                        String blackUsername = rs.getString("blackUsername");
+                        String gameName = rs.getString("gameName");
+                        String chessGame = rs.getString("chessGame");
+                        ChessGame game = new Gson().fromJson(chessGame, ChessGame.class);
+                        result.add(new GameData(id, whiteUsername, blackUsername, gameName, game));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return result;
     }
 
     @Override
     public ArrayList<GameDataShort> listGamesForResponse() throws DataAccessException {
-        return null;
+        var result = new ArrayList<GameDataShort>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM games";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        int id = rs.getInt("gameID");
+                        String whiteUsername = rs.getString("whiteUsername");
+                        String blackUsername = rs.getString("blackUsername");
+                        String gameName = rs.getString("gameName");
+                        result.add(new GameDataShort(id, whiteUsername, blackUsername, gameName));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return result;
     }
 
     @Override
@@ -46,6 +96,24 @@ public class SQLGameDAO implements GameDataAccess{
 
     @Override
     public void clear() throws DataAccessException {
+        var statement = "TRUNCATE games";
+        execute(statement);
+    }
 
+    private void execute(String statement, Object... things) throws  DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var prep = conn.prepareStatement(statement)) {
+                for (var i = 0; i < things.length; i++) {
+                    var param = things[i];
+                    if (param instanceof String p) prep.setString(i + 1, p);
+                    else if (param instanceof Integer p) prep.setInt(i + 1, p);
+                    else if (param instanceof ChessGame p) prep.setString(i + 1, new Gson().toJson(p));
+                    else if (param == null) prep.setNull(i + 1, NULL);
+                }
+                prep.executeUpdate();
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 }
